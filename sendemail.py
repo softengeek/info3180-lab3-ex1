5827fdf6502fc522606f7b3b3998a450b34b4f7c
softengeek <softengeekkhadz@gmail.com>
2017-02-13 00:39:34 +0000

    made changes

--- sendemail.py
+++ sendemail.py
@@ -10,13 +10,12 @@ To: {} <{}>
 Subject: {} 
 
 {}
-""".format(from_name, from_addr, to_name, to_addr, subject, msg)
-
+"""
 
 message_to_send = message.format(from_name, from_addr, to_name, to_addr, subject, msg)
 # Credentials (if needed)
-username = ""
-password = ""
+username = ''
+password = ''
 # The actual mail send
 server = smtplib.SMTP('smtp.gmail.com:587')
 server.starttls()
